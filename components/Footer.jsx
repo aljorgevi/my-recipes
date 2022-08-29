c0ff21f5016631f2a8ba90788b178153a5ad77b7
aljorgevi <jormencar@yahoo.com>
2022-08-29 14:46:54 +0100

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+		<footer className='page-footer'>
+			<p>
+				&copy; {new Date().getFullYear()} <span>Recipes Fiesta</span> Developed by
+				<a href='https://aljorgevi.com'> aljorgevi</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
